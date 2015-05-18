afd16ed69de9cbd2581745c62c9e7d69deb375bd
shinznatkid <shinznatkid@gmail.com>
2015-05-19 01:25:14 +0700

    - Include pagination

--- djangomyadmin/views.py
+++ djangomyadmin/views.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from .database import Database, InvalidUsernameorpassword
 from .models import *
 from django.http import HttpResponse
+from django.core.urlresolvers import resolve, reverse
 import json
+import math
 
 
 def dblogin_required(view_func):
@@ -183,28 +185,62 @@ def ajax_create_table(request, database_name):
 
 @dblogin_required
 def page_table(request, database_name, table_name):
+    current_url_resolve = resolve(request.path_info)
+    current_url_name = current_url_resolve.url_name
+    current_url = reverse(current_url_name, args=current_url_resolve.args, kwargs=current_url_resolve.kwargs)
+
     pos = int(request.GET.get('pos', 0))
-    username = request.session.get('username')
-    password = request.session.get('password')
-    db = Database(username, password, database_name)
-    columns = db.get_columns(table_name)
-    query = 'SELECT * FROM `%s` LIMIT %s , 30' % (table_name, pos)
-    records = db.display_query(query)
+    if pos < 0:
+        pos = 0
+    max_rows = int(request.GET.get('max_rows', 30))
+    if max_rows < 1:
+        max_rows = 30
+
+    username         = request.session.get('username')
+    password         = request.session.get('password')
+    db               = Database(username, password, database_name)
+    columns          = db.get_columns(table_name)
+    query            = 'SELECT * FROM `%s` LIMIT %s , %s' % (table_name, pos, max_rows)
+    records          = db.display_query(query)
     query_time_usage = db.query_time
-    table_engine = Tables.objects.using('schema').filter(table_schema=database_name, table_name=table_name).first().engine
-    count = db.query('EXPLAIN SELECT * FROM `%s`' % (table_name))[0][8]
+    table_engine     = Tables.objects.using('schema').filter(table_schema=database_name, table_name=table_name).first().engine
+    count            = db.query('EXPLAIN SELECT * FROM `%s`' % (table_name))[0][8]
+    count_str        = str(count)
     if table_engine == 'InnoDB':
-        count = '~%d' % count
+        count_str = '~%d' % count
+
+    nav_current_page = int(math.floor(pos / max_rows)) + 1
+    latest_page      = int(math.floor(count / max_rows)) + 1
+    nav_prev_page     = None
+    nav_next_page     = None
+    nav_first_page    = None
+    nav_last_page     = None
+    if nav_current_page > 1:
+        nav_prev_page = (nav_current_page - 2) * max_rows if nav_current_page > 2 else 0
+    if nav_current_page < latest_page:
+        nav_next_page = nav_current_page * max_rows
+    if nav_current_page > 2:
+        nav_first_page = 0
+    if nav_current_page < latest_page:
+        nav_last_page = (latest_page - 1) * max_rows if latest_page > 0 else 0
+
     data = {
         'columns': columns,
         'records': records,
-        'count': count,
+        'count': count_str,
         'query': query,
         'query_time_usage': '%.4f' % query_time_usage,
         'database_name': database_name,
         'table_name': table_name,
         'pos': pos,
         'pos_end': pos + len(records),
+
+        'nav_current_page': nav_current_page,
+        'nav_prev_page': nav_prev_page,
+        'nav_next_page': nav_next_page,
+        'nav_first_page': nav_first_page,
+        'nav_last_page': nav_last_page,
+        'current_url': current_url,
     }
     return render(request, 'page/sql.html', data)
 
