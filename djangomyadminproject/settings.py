ef33703ccf778da257d71c4449302b4e6b045916
shinznatkid <shinznatkid@gmail.com>
2015-05-22 15:44:57 +0700

    - update requirements - include static root for collectstatic

--- djangomyadminproject/settings.py
+++ djangomyadminproject/settings.py
@@ -90,6 +90,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 PICKER_INSTALLED_APPS = (
     'jquery',
